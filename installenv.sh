7bdd4a16a52773cb0e54fe75c5a665a0aa6b363e
rockwell15@hotmail.com <rockwell15@hotmail.com>
2016-04-13 21:54:36 -0400

    adding bash file

---
+++ installenv.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+SSH_PORT="22"
+SITE_NAME="" # optional - site name
+DB_PASSWORD="database_password"
+DB_NAME="database_name"
+DB_USER="database_user"
+DB_USER_PASSWORD="database_user_password"
+
+
+
+###########################################################
+# Functions
+###########################################################
+
+#---- Install aptitude & update
+
+function system_update {
+    apt-get update
+    apt-get -y install aptitude
+    aptitude -y full-upgrade
+}
+
+
+
+#---- Install postfix
+
+function postfix_install_loopback_only {
+    # Installs postfix and configure to listen only on the local interface. Also
+    # allows for local mail delivery
+
+    echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
+    echo "postfix postfix/mailname string localhost" | debconf-set-selections
+    echo "postfix postfix/destinations string localhost.localdomain, localhost" | debconf-set-selections
+    aptitude -y install postfix
+    /usr/sbin/postconf -e "inet_interfaces = loopback-only"
+    #/usr/sbin/postconf -e "local_transport = error:local delivery is disabled"
+
+    touch /tmp/restart-postfix
+}
+
+
+
+#---- MySQL stuff
+
+function mysql_install {
+    # $1 - the mysql root password
+
+    if [ ! -n "$1" ]; then
+        echo "mysql_install() requires the root pass as its first argument"
+        return 1;
+    fi
+
+    echo "mysql-server mysql-server/root_password password $1" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password $1" | debconf-set-selections
+    apt-get -y install mysql-server mysql-client
+
+    echo "Sleeping while MySQL starts up for the first time..."
+    sleep 5
+}
+
+function mysql_tune {
+    # Tunes MySQL's memory usage to utilize the percentage of memory you specify, defaulting to 40%
+
+    # $1 - the percent of system memory to allocate towards MySQL
+
+    if [ ! -n "$1" ];
+        then PERCENT=40
+        else PERCENT="$1"
+    fi
+
+    sed -i -e 's/^#skip-innodb/skip-innodb/' /etc/mysql/my.cnf # disable innodb - saves about 100M
+
+    MEM=$(awk '/MemTotal/ {print int($2/1024)}' /proc/meminfo) # how much memory in MB this system has
+    MYMEM=$((MEM*PERCENT/100)) # how much memory we'd like to tune mysql with
+    MYMEMCHUNKS=$((MYMEM/4)) # how many 4MB chunks we have to play with
+
+    # mysql config options we want to set to the percentages in the second list, respectively
+    OPTLIST=(key_buffer sort_buffer_size read_buffer_size read_rnd_buffer_size myisam_sort_buffer_size query_cache_size)
+    DISTLIST=(75 1 1 1 5 15)
+
+    for opt in ${OPTLIST[@]}; do
+        sed -i -e "/\[mysqld\]/,/\[.*\]/s/^$opt/#$opt/" /etc/mysql/my.cnf
+    done
+
+    for i in ${!OPTLIST[*]}; do
+        val=$(echo | awk "{print int((${DISTLIST[$i]} * $MYMEMCHUNKS/100))*4}")
+        if [ $val -lt 4 ]
+            then val=4
+        fi
+        config="${config}\n${OPTLIST[$i]} = ${val}M"
+    done
+
+    sed -i -e "s/\(\[mysqld\]\)/\1\n$config\n/" /etc/mysql/my.cnf
+
+    touch /tmp/restart-mysql
+}
+
+function mysql_create_database {
+    # $1 - the mysql root password
+    # $2 - the db name to create
+
+    if [ ! -n "$1" ]; then
+        echo "mysql_create_database() requires the root pass as its first argument"
+        return 1;
+    fi
+    if [ ! -n "$2" ]; then
+        echo "mysql_create_database() requires the name of the database as the second argument"
+        return 1;
+    fi
+
+    echo "CREATE DATABASE $2;" | mysql -u root -p$1
+}
+
+function mysql_create_user {
+    # $1 - the mysql root password
+    # $2 - the user to create
+    # $3 - their password
+
+    if [ ! -n "$1" ]; then
+        echo "mysql_create_user() requires the root pass as its first argument"
+        return 1;
+    fi
+    if [ ! -n "$2" ]; then
+        echo "mysql_create_user() requires username as the second argument"
+        return 1;
+    fi
+    if [ ! -n "$3" ]; then
+        echo "mysql_create_user() requires a password as the third argument"
+        return 1;
+    fi
+
+    echo "CREATE USER '$2'@'localhost' IDENTIFIED BY '$3';" | mysql -u root -p$1
+}
+
+function mysql_grant_user {
+    # $1 - the mysql root password
+    # $2 - the user to bestow privileges 
+    # $3 - the database
+
+    if [ ! -n "$1" ]; then
+        echo "mysql_create_user() requires the root pass as its first argument"
+        return 1;
+    fi
+    if [ ! -n "$2" ]; then
+        echo "mysql_create_user() requires username as the second argument"
+        return 1;
+    fi
+    if [ ! -n "$3" ]; then
+        echo "mysql_create_user() requires a database as the third argument"
+        return 1;
+    fi
+
+    echo "GRANT ALL PRIVILEGES ON $3.* TO '$2'@'localhost';" | mysql -u root -p$1
+    echo "FLUSH PRIVILEGES;" | mysql -u root -p$1
+
+}
+
+
+
+#---- Restart services
+
+function restartServices {
+    # restarts services that have a file in /tmp/needs-restart/
+
+    for service in $(ls /tmp/restart-* | cut -d- -f2-10); do
+        /etc/init.d/$service restart
+        rm -f /tmp/restart-$service
+    done
+}
+
+
+
+###########################################################
+# Command stuffs
+###########################################################
+
+#---- Update apt
+system_update
+
+
+
+#---- Install postfix mail
+postfix_install_loopback_only
+
+
+
+#---- Change default SSH port
+sed -i 's/Port 22/Port '$SSH_PORT'/g' /etc/ssh/sshd_config
+
+
+
+#---- Login security
+apt-get -y install fail2ban
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+perl -i -0pe 's/\[ssh-ddos\]\n\nenabled  = false\nport     = ssh/\[ssh-ddos\]\n\nenabled  = true\nport     = '$SSH_PORT'/mg' /etc/fail2ban/jail.local
+service fail2ban restart
+
+
+
+#---- Reboot when out-of-memory
+echo '' >> /etc/sysctl.conf
+echo 'vm.panic_on_oom=1' >> /etc/sysctl.conf
+echo 'kernel.panic=10' >> /etc/sysctl.conf
+
+
+
+#---- Install PHP
+apt-get install software-properties-common -y --force-yes
+add-apt-repository ppa:ondrej/php -y
+apt-get update
+apt-get install -y --force-yes php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json
+
+
+
+#---- Install MySQL
+mysql_install "$DB_PASSWORD" && mysql_tune 40
+mysql_create_database "$DB_PASSWORD" "$DB_NAME"
+mysql_create_user "$DB_PASSWORD" "$DB_USER" "$DB_USER_PASSWORD"
+mysql_grant_user "$DB_PASSWORD" "$DB_USER" "$DB_NAME"
+
+
+
+#---- Install Apache
+apt-get install apache2 -y
+
+cp /etc/apache2/apache2.conf /etc/apache2/apache2.backup.conf
+sed -i 's/KeepAlive On/KeepAlive Off/' /etc/apache2/apache2.conf
+
+
+
+#---- Restart everything
+restartServices
